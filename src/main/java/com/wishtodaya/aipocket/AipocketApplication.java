d17061d63a8c9089896a5ce263c0ff0de4a8218b
zheng <zhengjunqiang@airlonghao.com>
2023-06-13 11:24:00 +0800

    添加SpringBoot框架

---
+++ src/main/java/com/wishtodaya/aipocket/AipocketApplication.java
@@ -0,0 +1,13 @@
+package com.wishtodaya.aipocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AipocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AipocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wishtodaya/aipocket/AipocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wishtodaya.aipocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AipocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
