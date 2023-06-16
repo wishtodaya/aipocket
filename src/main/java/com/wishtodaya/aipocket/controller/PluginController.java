d86186591be3869f5fd88ed96ebf8b26e3fc04c7
wishtodaya <wishtodaya@gmail.com>
2023-06-16 12:31:58 +0800

    增加swgger文档

--- src/main/java/com/wishtodaya/aipocket/controller/PluginController.java
+++ src/main/java/com/wishtodaya/aipocket/controller/PluginController.java
@@ -21,11 +21,19 @@ public class PluginController {
     private final CategoryService categoryService;
 
     @Autowired
-    public PluginController(PluginService pluginService,CategoryService categoryService) {
+    public PluginController(PluginService pluginService, CategoryService categoryService) {
         this.pluginService = pluginService;
-        this.categoryService=categoryService;
+        this.categoryService = categoryService;
     }
 
+    /**
+     * 获取指定分类下的插件列表
+     *
+     * @param pageNumber 页码，用于分页查询
+     * @param pageSize   每页的记录数，用于分页查询
+     * @param categoryId 分类ID，用于指定插件分类
+     * @return 返回一个Result类型的响应，包含插件的分页信息
+     */
     @GetMapping("/plugins")
     public Result getPluginsByCategory(@RequestParam int pageNumber, @RequestParam int pageSize, @RequestParam String categoryId) {
         Page<Plugin> pagePlugins = pluginService.getPluginsByCategory(pageNumber, pageSize, categoryId);
@@ -34,6 +42,11 @@ public class PluginController {
         return result;
     }
 
+    /**
+     * 获取所有分类的列表
+     *
+     * @return 返回一个Result类型的响应，包含所有的分类信息
+     */
     @GetMapping("/categorys")
     public Result getAllCategorys() {
         List<Category> categorys = categoryService.getCategorys();
